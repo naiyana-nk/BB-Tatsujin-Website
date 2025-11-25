806349a5ecc7988e3435ac4c3e20cab4afa29a3f
Naiyana Norkaew <naiyana.nk14@gmail.com>
2025-11-25 13:19:33 +0700

    remove header & footer to make it myself

--- app/header/page.tsx
+++ app/header/page.tsx
@@ -6,6 +6,7 @@ import { Menu, X } from "lucide-react";
 import './header.css'
 
 export default function Header() {
+  // For mobile
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const navItems = [
@@ -16,55 +17,7 @@ export default function Header() {
 
   return (
     <header className="header">
-      <nav className="header-nav">
-        <div className="header-container">
-          {/* Logo */}
-          <div className="header-logo">
-            <Link href="/">
-              <img src="./assets/BeBlock_Bangkok_Logo-02.png" alt="BeBlock Logo" className="logo-img" />
-            </Link>
-          </div>
 
-          {/* Desktop Navigation */}
-          <div className="nav-desktop">
-            {navItems.map((item) => (
-              <Link key={item.name} href={item.href} className="nav-link">
-                {item.name}
-              </Link>
-            ))}
-            <button className="btn-primary">Get Started</button>
-          </div>
-
-          {/* Mobile menu button */}
-          <div className="nav-mobile-toggle">
-            <button
-              onClick={() => setIsMenuOpen(!isMenuOpen)}
-              className="menu-button"
-            >
-              {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
-            </button>
-          </div>
-        </div>
-
-        {/* Mobile Navigation */}
-        {isMenuOpen && (
-          <div className="nav-mobile">
-            <div className="nav-mobile-items">
-              {navItems.map((item) => (
-                <Link
-                  key={item.name}
-                  href={item.href}
-                  className="nav-link-mobile"
-                  onClick={() => setIsMenuOpen(false)}
-                >
-                  {item.name}
-                </Link>
-              ))}
-              <button className="btn-primary-mobile">Get Started</button>
-            </div>
-          </div>
-        )}
-      </nav>
     </header>
   );
 }
