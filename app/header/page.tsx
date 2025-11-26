63190ced1261d562b7c4ed6f5ea4f6e32e207357
Naiyana Norkaew <naiyana.nk14@gmail.com>
2025-11-26 11:18:30 +0700

    re design header

--- app/header/page.tsx
+++ app/header/page.tsx
@@ -2,22 +2,184 @@
 
 import { useState } from "react";
 import Link from "next/link";
-import { Menu, X } from "lucide-react";
-import './header.css'
+import "./header.css";
 
 export default function Header() {
-  // For mobile
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const navItems = [
-    { name: "Home", href: "/" },
-    { name: "Product", href: "/product" },
-    { name: "Contact", href: "/contact" },
-  ];
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
 
   return (
     <header className="header">
+      <div className="container">
+        <Link href="/" className="logo" onClick={closeMenu}>
+          <img src="./assets/LOGO_4.png" alt="BeBlock" />
+        </Link>
 
+        {/*  */}
+        <nav className={`nav ${isMenuOpen ? "nav-open" : ""}`}>
+          <Link href="/" className="nav-link" onClick={closeMenu}>
+            Home
+          </Link>
+          <Link href="/product" className="nav-link" onClick={closeMenu}>
+            Product
+          </Link>
+          <Link href="/" className="nav-link" onClick={closeMenu}>
+            Creation Guide
+          </Link>
+          <Link href="/" className="nav-link" onClick={closeMenu}>
+            Template
+          </Link>
+          <Link href="/contact" className="nav-link" onClick={closeMenu}>
+            Contact
+          </Link>
+          <Link
+            className="shopping-cart shopping-cart-mobile"
+            href="/"
+            onClick={closeMenu}
+          >
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="lucide lucide-shopping-basket-icon lucide-shopping-basket"
+            >
+              <path d="m15 11-1 9" />
+              <path d="m19 11-4-7" />
+              <path d="M2 11h20" />
+              <path d="m3.5 11 1.6 7.4a2 2 0 0 0 2 1.6h9.8a2 2 0 0 0 2-1.6l1.7-7.4" />
+              <path d="M4.5 15.5h15" />
+              <path d="m5 11 4-7" />
+              <path d="m9 11 1 9" />
+            </svg>
+            <p>Cart</p>
+          </Link>
+          <button
+            className="cta-button-mobile login-mobile flex items-center gap-2"
+            onClick={closeMenu}
+          >
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="lucide lucide-circle-user-round-icon lucide-circle-user-round"
+            >
+              <path d="M18 20a6 6 0 0 0-12 0" />
+              <circle cx="12" cy="10" r="4" />
+              <circle cx="12" cy="12" r="10" />
+            </svg>
+            Login
+          </button>
+          <button
+            className="cta-button-mobile regis-mobile flex items-center gap-2"
+            onClick={closeMenu}
+          >
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="lucide lucide-circle-user-round-icon lucide-circle-user-round"
+            >
+              <path d="M18 20a6 6 0 0 0-12 0" />
+              <circle cx="12" cy="10" r="4" />
+              <circle cx="12" cy="12" r="10" />
+            </svg>
+            Register
+          </button>
+        </nav>
+
+        <Link className="shopping-cart" href="/product">
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="lucide lucide-shopping-basket-icon lucide-shopping-basket"
+          >
+            <path d="m15 11-1 9" />
+            <path d="m19 11-4-7" />
+            <path d="M2 11h20" />
+            <path d="m3.5 11 1.6 7.4a2 2 0 0 0 2 1.6h9.8a2 2 0 0 0 2-1.6l1.7-7.4" />
+            <path d="M4.5 15.5h15" />
+            <path d="m5 11 4-7" />
+            <path d="m9 11 1 9" />
+          </svg>
+          <p>Cart</p>
+        </Link> 
+
+        <button className="cta-button login-btn flex items-center gap-2">
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="lucide lucide-circle-user-round-icon lucide-circle-user-round"
+          >
+            <path d="M18 20a6 6 0 0 0-12 0" />
+            <circle cx="12" cy="10" r="4" />
+            <circle cx="12" cy="12" r="10" />
+          </svg>
+          Login
+        </button>
+
+        <button className="cta-button regis-btn flex items-center gap-2">
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="lucide lucide-circle-user-round-icon lucide-circle-user-round"
+          >
+            <path d="M18 20a6 6 0 0 0-12 0" />
+            <circle cx="12" cy="10" r="4" />
+            <circle cx="12" cy="12" r="10" />
+          </svg>
+          Sign up
+        </button>
+
+        <button
+          className={`hamburger ${isMenuOpen ? "hamburger-open" : ""}`}
+          onClick={toggleMenu}
+          aria-label="Toggle menu"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+      </div>
     </header>
   );
 }
